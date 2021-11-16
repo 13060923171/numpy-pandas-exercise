ce40fd5fab312783a2d2542d7c476ba4f235f9c1
hao <960751327@qq.com>
2021-11-16 16:49:40 +0800

    新增项目

--- 数据处理实战案例4/main1.py
+++ 数据处理实战案例4/main1.py
@@ -8,6 +8,7 @@ df = pd.read_csv('rating_new.csv')
 uid = list(df.user_new.values)
 timedata = list(df.time.values)
 iid = list(df.item_new.values)
+new_iid = list(df.index)
 
 count = 0
 iid_list = []
@@ -32,9 +33,17 @@ while count+1 < len(iid):
     uid_list = []
     count += 1
 
+sum_iid1 = []
+sum_uid1 = []
+for u,i in zip(sum_uid,sum_iid):
+    if len(u) >= 2 and len(i) >= 2:
+        sum_uid1.append(u)
+        sum_iid1.append(i)
+
+
 df = pd.DataFrame()
-uid_1 = tqdm(list(chain(*sum_uid)))
-iid_1 = tqdm(list(chain(*sum_iid)))
+uid_1 = tqdm(list(chain(*sum_uid1)))
+iid_1 = tqdm(list(chain(*sum_iid1)))
 df['uid'] = uid_1
 df['iid'] = iid_1
 df.to_csv('item_link.csv')
@@ -42,12 +51,12 @@ df.to_csv('item_link.csv')
 #     df['uid'] = sum_uid[i]
 #     df['iid'] = sum_iid[i]
 #     df.to_csv('item_link.csv',mode='a',index=None,header=None)
-    # j = sum_iid[i]
-    # j1 = sum_iid[i+1]
-    # k = sum_uid[i]
-    # k1 = sum_uid[i+1]
-    # if k[0] == k1[0]:
-    #     j2 = j + j1
-    #     with open('iid_link.txt', 'a', encoding='utf-8')as f:
-    #         a = ' '.join('%s' % id for id in j2)
-    #         f.write(a + '\n')
+#     j = sum_iid[i]
+#     j1 = sum_iid[i+1]
+#     k = sum_uid[i]
+#     k1 = sum_uid[i+1]
+#     if k[0] == k1[0]:
+#         j2 = j + j1
+#         with open('iid_link.txt', 'a', encoding='utf-8')as f:
+#             a = ' '.join('%s' % id for id in j2)
+#             f.write(a + '\n')
